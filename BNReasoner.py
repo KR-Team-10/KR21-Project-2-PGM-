b053b68ceba13218c21ac828603eba33ddca2d11
Martín Ignacio Pucheu Avilés <martin.pucheuaviles@gmail.com>
2021-12-17 17:57:46 +0100

    elimination order

--- BNReasoner.py
+++ BNReasoner.py
@@ -1,6 +1,7 @@
 from typing import Union, List, Tuple, Dict
 from BayesNet import BayesNet
-
+from itertools import combinations
+import copy
 
 class BNReasoner:
     def __init__(self, net: Union[str, BayesNet]):
@@ -89,8 +90,90 @@ class BNReasoner:
     # TODO liberate the settlements
     def MEP(self):
         pass
+    
+    #Get the MINIMAL DEGREE order of variable eliminiation
+    def min_degree_order(self):
+        interaction = self.bn.get_interaction_graph()
+        pi = []
+        for i in range(0,len(interaction.nodes)):
+            print("interaction.nodes: ", interaction.nodes)        
+            print("interaction.edges: ", interaction.edges)
 
+            #get node with min degree
+            degrees = {}
+            for node in interaction.nodes:
+                degrees[node] = 0 
+                for a,b in interaction.edges:
+                    if a == node or b == node:
+                        degrees[node] += 1
+            
+            print("node degrees: ", degrees)
+            min_degree_node = min(degrees, key=degrees.get)
+            pi.append(min_degree_node)
+            
+            #get neighbors of min grade node
+            neighbors = []
+            for a,b in interaction.edges:
+                if a == min_degree_node: 
+                    neighbors.append(b)
+                elif b == min_degree_node:
+                    neighbors.append(a)
+            print("\tmin_degree_node: {}\t-->neighbors: {}\n".format(min_degree_node,neighbors))
+            
+            #add an edge between every pair of non-adjacent neighbors of min_degree_node
+            if len(neighbors) > 1:
+                for comb in list(combinations(neighbors, 2)):
+                    interaction.add_edge(comb[0],comb[1]) #if the edge already exists, then this don't do anything
+            
+            #delete variable min_degree_node 
+            interaction.remove_node(min_degree_node)
 
+        print("Min Degree order PI= ", pi)
+        return pi
+
+    #Get the MINIMAL FILL order of variable eliminiation
+    def min_fill_order(self):
+        interaction = self.bn.get_interaction_graph()
+        pi = []
+        
+        for i in range(0,len(interaction.nodes)):
+            # print("nodescls = ",interaction.nodes)
+            counter = {}
+            add_edges = {}
+            for node in interaction.nodes:
+
+                add_edges[node] = []
+                neighbors = []
+                for a,b in interaction.edges:
+                    if a == node: 
+                        neighbors.append(b)
+                    elif b == node:
+                        neighbors.append(a)
+                
+                #count how many edges needs to be created if this variable is removed
+                counter[node] = 0
+                if len(neighbors) > 1:
+                    for comb in list(combinations(neighbors, 2)):
+                        if comb not in interaction.edges or tuple(reversed(comb)) not in (interaction.edges):
+                            counter[node] += 1
+                            add_edges[node].append(comb)
+                
+            #get the node which add the min number of edges if it is removed
+            min_fill_node = min(counter, key=counter.get)
+                
+            #add an edge between every pair of non-adjacent neighbors of min_degree_node
+            if len(add_edges[min_fill_node]) > 0: 
+                for ne in add_edges[min_fill_node]:
+                    interaction.add_edge(ne[0],ne[1])
+
+            #delete variable min_degree_node 
+            interaction.remove_node(min_fill_node)
+            pi.append(min_fill_node)
+            
+        print("Min FILL order PI = ", pi)
+        return pi
+
+    
 # Mainly for trying things
 def main():
     # net_path = "testing/dog_problem.BIFXML"
@@ -99,22 +182,28 @@ def main():
 
     # reasoner.bn.draw_structure()
 
-    variables = list("ABCDEFG")
+    variables = list("ABCDE")
     edges = [
         ("A", "B"),
         ("A", "C"),
+        ("B", "D"),
         ("C", "D"),
-        ("D", "E"),
-        ("D", "F"),
-        ("F", "G"),
-        ("A", "G"),
-        ("B", "E"),
-        ("D", "G"),
+        ("C", "E"),
     ]
     bn = BayesNet()
     bn.create_bn(variables, edges, {x: None for x in variables})
-    bn.draw_structure()
+    # bn.draw_structure()
 
+    main_martin()
 
+def main_martin():
+    net_path = "testing/interaction_graph_example.BIFXML"
+
+    reasoner = BNReasoner(net=net_path)
+    
+    # reasoner.min_degree_order()
+    # reasoner.min_fill_order()
+
+    
 if __name__ == "__main__":
     main()
